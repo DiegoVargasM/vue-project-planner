507f543894c12a7186208aebd850fc9cf0b497da
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-11-15 08:35:20 -0500

    Create SingleProject comp. template and bind data

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="project">
+    <div class="actions">
+      <h3>{{ project.title }}</h3>
+    </div>
+    <div class="details">
+      <p>{{ project.details }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SingleProject",
+  props: ["project"],
+};
+</script>
+
+<style scoped></style>
