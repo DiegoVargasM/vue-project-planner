57449c6a1c8dea4b08cfb177d58ebde240ea92ba
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-11-15 08:42:19 -0500

    Add initial styling  to SingleProject comp

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -16,4 +16,16 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.project {
+  background: white;
+  margin: 20px auto;
+  padding: 10px 20px;
+  border-radius: 4px;
+  box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.12);
+  border-left: 4px solid rgb(240, 20, 64);
+}
+h3 {
+  cursor: pointer;
+}
+</style>
