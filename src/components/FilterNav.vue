d7368470d971dfc53dfdd6740307317d0fcd38bf
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-11-16 11:27:10 -0500

    Add styles to FilterNav buttons

--- src/components/FilterNav.vue
+++ src/components/FilterNav.vue
@@ -17,4 +17,20 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.filter-nav button {
+  background: none;
+  border: none;
+  color: #9e9d9d;
+  outline: none;
+  font-size: 0.8rem;
+  text-transform: uppercase;
+  margin-right: 10px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  cursor: pointer;
+}
+.filter-nav button.active {
+  color: #424242;
+}
+</style>
