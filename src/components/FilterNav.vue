b6a33d92536f407859b0818411067f2c57ca62d0
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-11-16 11:14:06 -0500

    Boilerplate FilterNav component

---
+++ src/components/FilterNav.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="filter-nav">
+    <button>View All</button>
+    <button>Completed</button>
+    <button>Ongoing</button>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "FilterNav",
+};
+</script>
+
+<style></style>
