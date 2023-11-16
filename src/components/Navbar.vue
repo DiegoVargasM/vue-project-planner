b7877c2d0d09df3e0d12e7d286f963b7cfa7ec49
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-11-16 10:21:28 -0500

    Create navbar and add to App.vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav>
+    <router-link :to="{ name: 'Home' }">Projects</router-link>
+    <router-link :to="{ name: 'AddProject' }">Add a New Project</router-link>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
